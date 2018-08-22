6a456fcf8db142fd6056293c5fe00ab1dca26855
Victor Chong <victor.chong@linaro.org>
2018-08-22 19:35:37 +0900

    km3: increase heap for TA to 2MB
    VtsHalKeymasterV3_0TargetTest is running out of memory when running
    RsaCorruptedKeyBlob and EcCorruptedKeyBlob tests.
    
    In generator.c:
    E/TA:  TA_restore_key:604 Failed to allocate memory for attribute

--- keymaster/ta/user_ta_header_defines.h
+++ keymaster/ta/user_ta_header_defines.h
@@ -12,7 +12,7 @@
 #define TA_FLAGS				(TA_FLAG_MULTI_SESSION | TA_FLAG_EXEC_DDR | \
 						TA_FLAG_SINGLE_INSTANCE)
 #define TA_STACK_SIZE			(2 * 1024)
-#define TA_DATA_SIZE			(256 * 1024)
+#define TA_DATA_SIZE			(2 * 1024 * 1024)
 
 #define TA_CURRENT_TA_EXT_PROPERTIES \
 	{ "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
